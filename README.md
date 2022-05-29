1b1302dc1800d6d37312f997ed847b94c9a4cbf5
Tomasz Lipinski <t.lipinski@op.pl>
2022-05-29 20:51:15 +0200

    color mixer

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Color Mixer
+
+To see Color Mixer demo type
+
+```bash
+  npm install && npm start
+```
+
+Color mixing algorithm was taken from 
+
+[Chroma.js](https://gka.github.io/chroma.js/#chroma-mix)
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-   const generateColorsSet = () => {
+(
+    function() {
+
+        const generateColorsSet = () => {
             const randomColors = []
-            for(let i = 0;i<100;i++) {
+            for(let i = 0;i<1000;i++) {
                 const randomColor = `#${Math.floor((Math.random()*1000000)+1).toString().padStart(6, '0')}`
                 if(randomColors.indexOf(randomColor) < 0) {
                     randomColors.push(randomColor)
@@ -8,38 +11,67 @@
             }
             return randomColors
         }
+        
+        const createColorButton = (color, primaryColor, container) => {
+            const btn = document.createElement("button")
+            btn.classList.add('colorButton')
+            btn.setAttribute('data-color', color)
+            btn.setAttribute('data-set', primaryColor ? 'primary' : 'secondary')
+            btn.style.backgroundColor = color
+            container.appendChild(btn);
+        }
+        
+        const generateColorButtons = (container, colors, primaryColor = true) => {
+            colors.map(color => createColorButton(color, primaryColor, container))
+        }
 
-        // const mixed = chroma.mix('#000000', '#ffffff', 0.25, 'rgb').hex()
-        // const clicker = document.getElementById('onClickHandler')
-        // clicker.addEventListener('click', e => {
-        //     const attr = e.target.attributes.getNamedItem('data-val')
-        //     if(attr) {
-        //     console.log(attr.value, mixed)
-        //     }
-        // })
+        const selectedColors = {
+            primary: '',
+            secondary: ''
+        }
 
+        const setColor = (value, set) => {
+            if(set === 'primary') {
+                selectedColors.primary = value
+            }
+            if(set === 'secondary') {
+                selectedColors.secondary = value
+            }
+        }
 
-const generateColorButtons = (elementId, colors) => {
-    const container = document.getElementById(elementId)
-    colors.map(element => {
-        const btn = document.createElement("button")
-        btn.classList.add('colorButton')
-        btn.setAttribute('data-color', element)
-        btn.style.backgroundColor = element
-        container.appendChild(btn);
-    })
-}  
-(
-    function() {
+        const setMixedColor = () => {
+            if(selectedColors.primary && selectedColors.secondary) {
+                mixedColorContainer.style.backgroundColor = chroma.mix(selectedColors.primary, selectedColors.secondary, 0.25, 'rgb').hex()
+            }
+        }
+
+        const setAsActive = element => {
+            const container = element.parentElement
+            const currentSelected = container.querySelector('.active')
+            if (currentSelected) {
+                currentSelected.classList.remove('active')
+            }
+            element.classList.add('active')
+        }
+
+        const handleClick = e => {
+            const color = e.target.attributes.getNamedItem('data-color')
+            const set = e.target.attributes.getNamedItem('data-set')
+            if(color && color.value && set && set.value) {
+                setColor(color.value, set.value)
+                setAsActive(e.target)
+                setMixedColor()
+            }
+        }
+
         const colorsSet = generateColorsSet()
+        const colorMixerElement = document.getElementById('colorMixer')
+        const mixedColorContainer = colorMixerElement.querySelector('.mixedColor')
 
-        generateColorButtons('primaryColors', colorsSet)
-        generateColorButtons('secondaryColors', colorsSet)
-
-
-
-
+        generateColorButtons(colorMixerElement.querySelector('.primaryColors'), colorsSet)
+        generateColorButtons(colorMixerElement.querySelector('.secondaryColors'), colorsSet, false)
+        
+        colorMixerElement.addEventListener('click', handleClick)
     }
 )()
-
 
